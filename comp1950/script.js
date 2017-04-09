1212c0f12c07d458fab4642918de1e32021b7deb
H. Julian Sloman <julix91@gmx.de>
2017-04-08 21:40:12 -0700

    Added sample lecture and auto ToC script

---
+++ comp1950/script.js
@@ -0,0 +1,50 @@
+$(function() {
+
+	String.prototype.repeat = function(num) {
+		return new Array(num + 1).join(this);
+	}
+	var ToC =
+		"<details><summary>Contents</summary><nav role='navigation' class='table-of-contents'>" +
+			"" +
+			"<ol>";
+
+	var newLine, el, title, link, level, baseLevel;
+
+	$("article h2, article h3, article h4, article h5, article h6").each(function() {
+
+		el = $(this);
+		title = el.text();
+		el.attr("id", title.split(' ').join('_'));
+		link = "#" + el.attr("id");
+
+		var prevLevel = level || 0;
+		level = this.nodeName.substr(1);
+		if(!baseLevel) { // make sure you start with highest level of heading or it won't work
+			baseLevel = level;
+		}
+
+
+
+		if(prevLevel == 0) {
+			newLine = '<li>';
+		} else if(level == prevLevel) {
+			newLine = '</li><li>';
+		} else if(level > prevLevel) {
+			newLine = '<ol><li>'.repeat(level - prevLevel);
+		} else if(level < prevLevel) {
+			newLine = '</li></ol>'.repeat(prevLevel - level) +
+			'</li><li>';
+		}
+		newLine += "<a href='" + link + "'>" + title + "</a>";
+
+		ToC += newLine;
+
+	});
+
+	ToC += '</li></ol>'.repeat(level - baseLevel) +
+				"</li>" +
+			"</ol>" +
+		"</nav></details>";
+
+	$("#toc-here").prepend(ToC);
+});
